fc46707a0262c82ec91060ec823008446fbfba11
Paul Bacchus <paulbacchus@M35-117.local>
2024-03-27 15:12:37 +0000

    Docker working.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '0.0.0.0:3000'],
+    },
+  },
+};
 
 module.exports = nextConfig;
